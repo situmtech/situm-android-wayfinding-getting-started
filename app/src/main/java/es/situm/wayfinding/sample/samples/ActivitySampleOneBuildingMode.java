845dab1d165356fb247c60b8827501aacb34b826
Adrian Martinez Regueira <adrian.martinez@situm.es>
2020-11-27 13:45:35 +0100

    Update One building mode example to do it programmatically

--- app/src/main/java/es/situm/wayfinding/sample/samples/ActivitySampleOneBuildingMode.java
+++ app/src/main/java/es/situm/wayfinding/sample/samples/ActivitySampleOneBuildingMode.java
@@ -3,13 +3,29 @@ package es.situm.wayfinding.sample.samples;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import es.situm.wayfinding.SitumMap;
+import es.situm.wayfinding.SitumMapView;
 import es.situm.wayfinding.sample.R;
 
-public class ActivitySampleOneBuildingMode extends AppCompatActivity {
+public class ActivitySampleOneBuildingMode extends AppCompatActivity implements SitumMapView.OnMapReadyCallback {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample_one_building_mode);
+        SitumMapView mapView = findViewById(R.id.map_view);
+        mapView.setOnMapReadyCallback(this);
+    }
+
+    @Override
+    public void onMapReady(SitumMap situmMap) {
+        //You must change the building id in the xml or the example won't work
+        situmMap.enableOneBuildingMode(getString(R.string.one_building_mode_building_id));
+    }
+
+    @Override
+    public void onError() {
+
     }
 }
