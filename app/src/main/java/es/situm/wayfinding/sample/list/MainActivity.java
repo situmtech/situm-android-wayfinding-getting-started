7905d6ef84bd806dd49d451d9c9a2192f607fc8b
Rodrigo Lago <rodrigo.lago@situm.com>
2021-12-01 09:58:32 +0100

    Update WYF version, add icons example

--- app/src/main/java/es/situm/wayfinding/sample/list/MainActivity.java
+++ app/src/main/java/es/situm/wayfinding/sample/list/MainActivity.java
@@ -2,27 +2,26 @@ package es.situm.wayfinding.sample.list;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import es.situm.wayfinding.sample.R;
 import es.situm.wayfinding.sample.samples.ActivitySampleAutoStartPositioning;
 import es.situm.wayfinding.sample.samples.ActivitySampleChangeGeofencesColor;
+import es.situm.wayfinding.sample.samples.ActivitySampleCustomPositionIcons;
 import es.situm.wayfinding.sample.samples.ActivitySampleCustomizeRequests;
 import es.situm.wayfinding.sample.samples.ActivitySampleCustomizeUI;
 import es.situm.wayfinding.sample.samples.ActivitySampleDashboardTheme;
 import es.situm.wayfinding.sample.samples.ActivitySampleDelegateBackEvent;
 import es.situm.wayfinding.sample.samples.ActivitySampleListenEvents;
 import es.situm.wayfinding.sample.samples.ActivitySampleNoSearchView;
-import es.situm.wayfinding.sample.samples.ActivitySampleSingleBuildingMode;
 import es.situm.wayfinding.sample.samples.ActivitySampleSimpleMap;
+import es.situm.wayfinding.sample.samples.ActivitySampleSingleBuildingMode;
 import es.situm.wayfinding.sample.samples.ActivitySampleUsingLibrary;
 
 public class MainActivity extends AppCompatActivity implements ViewHolderHelper.OnSampleClickListener {
-
-    private RecyclerView mRecyclerView;
-    private ViewHolderHelper mSamplesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +30,8 @@ public class MainActivity extends AppCompatActivity implements ViewHolderHelper.
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mRecyclerView = findViewById(R.id.samples_recycler_view);
-        mSamplesList = new ViewHolderHelper();
+        RecyclerView mRecyclerView = findViewById(R.id.samples_recycler_view);
+        ViewHolderHelper mSamplesList = new ViewHolderHelper();
         mSamplesList.init(this, mRecyclerView, this);
 
         mSamplesList.addItem(R.string.situm_sample_title_simple_map, ActivitySampleSimpleMap.class);
@@ -41,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements ViewHolderHelper.
         mSamplesList.addItem(R.string.situm_sample_title_delegate_back_events, ActivitySampleDelegateBackEvent.class);
         mSamplesList.addItem(R.string.situm_sample_title_no_search_view, ActivitySampleNoSearchView.class);
         mSamplesList.addItem(R.string.situm_sample_title_dashboard_theme, ActivitySampleDashboardTheme.class);
+        mSamplesList.addItem(R.string.situm_sample_title_custom_position_icons, ActivitySampleCustomPositionIcons.class);
         mSamplesList.addItem(R.string.situm_sample_title_interceptors, ActivitySampleCustomizeRequests.class);
         mSamplesList.addItem(R.string.situm_sample_title_customize_ui, ActivitySampleCustomizeUI.class);
         mSamplesList.addItem(R.string.situm_sample_title_auto_start_positioning, ActivitySampleAutoStartPositioning.class);
